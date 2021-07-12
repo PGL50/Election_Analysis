e31135792f2773131800c750ecb89400aa8eb6dc
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-07-12 17:59:09 -0500

    Addin more to Python_Practice file

--- Python_practice.py
+++ Python_practice.py
@@ -1,25 +1,49 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
+# # lists
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
 
-temperature = int(input("What is the temperature outside? "))
+# #User input
+# temperature = int(input("What is the temperature outside? "))
 
-if temperature > 80:
-    print("Turn on the AC.")
-else:
-    print("Open the windows.")
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
 
-# What is the score?
-score = int(input("What is your test score? "))
+# #User input and elif statements
+# # What is the score?
+# score = int(input("What is your test score? "))
 
-# Determine the grade.
-if score >= 90:
-    print('Your grade is an A.')
-elif score >= 80:
-    print('Your grade is a B.')
-elif score >= 70:
-    print('Your grade is a C.')
-elif score >= 60:
-    print('Your grade is a D.')
-else:
-    print('Your grade is an F.')
+# # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# elif score >= 80:
+#     print('Your grade is a B.')
+# elif score >= 70:
+#     print('Your grade is a C.')
+# elif score >= 60:
+#     print('Your grade is a D.')
+# else:
+#     print('Your grade is an F.')
+
+# # Calculations
+# # How many votes did you get?
+# my_votes = int(input("How many votes did you get in the election? "))
+# #  Total votes in the election
+# total_votes = int(input("What is the total votes in the election? "))
+# # Calculate the percentage of votes you received.
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not the list of counties.")
+
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
