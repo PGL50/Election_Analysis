0f155f1cd77af56409ebb0ea50bb29654662dcb3
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-07-12 19:02:37 -0500

    Addin more to Python_Practice file

--- Python_practice.py
+++ Python_practice.py
@@ -1,5 +1,5 @@
 # # lists
-# counties = ["Arapahoe","Denver","Jefferson"]
+counties = ["Arapahoe","Denver","Jefferson"]
 # if counties[1] == 'Denver':
 #     print(counties[1])
 
@@ -43,7 +43,42 @@
 #     print("El Paso is not the list of counties.")
 
 
-x = 0
-while x <= 5:
-    print(x)
-    x = x + 1
+# x = 0
+# while x <= 5:
+#     print(x)
+#     x = x + 1
+
+# for county in counties:
+#     print(county)
+
+# for num in range(5):
+#     print(num)
+
+# for i in range(len(counties)):
+#     print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+# for county in counties_dict:
+#     print(county)
+
+# for county in counties_dict.keys():
+#     print(county)
+
+# for voters in counties_dict.values():
+#     print(voters)
+
+# for county in counties_dict:
+#     print(counties_dict[county])
+
+# for voters in counties_dict:
+#     print(counties_dict[voters])
+
+# for county in counties_dict:
+#     print(counties_dict.get(county))
+
+# for county, voters in counties_dict.items():
+#     print(county, voters)
+
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters")
