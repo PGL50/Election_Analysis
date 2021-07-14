4dd56bfaec2270742dd482302ce0c732628a1e22
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-07-13 20:43:09 -0500

    Add analysis folder

--- PyPoll.py
+++ PyPoll.py
@@ -5,3 +5,19 @@
 # 4. The total number of votes each candidate won
 # 5. THe winner of the election based on the popular vote
 
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+#file_to_load = 'Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file.
+#election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
+
+# To do: perform analysis.
+    print(election_data)
+
+# Close the file.
+election_data.close()
