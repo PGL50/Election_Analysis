dd6f2906fc901305510a0d0e224e1d23db82a62a
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-07-13 20:19:50 -0500

    Add PyPloo.py

---
+++ PyPoll.py
@@ -0,0 +1,7 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of canditates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. THe winner of the election based on the popular vote
+
